4fbe719bddb74a2f398d6fadccaeeadf4278669b
KatyaEspinosa03 <katyamiranda03@hotmail.com>
2023-11-12 18:26:56 -0600

    Initial Commit

---
+++ MymovieApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MymovieApp
+//
+//  Created by Katya Miranda on 12/11/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
