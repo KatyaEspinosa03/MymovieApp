4b0d3cc50de901a0ecb12f26748a6a58d9bae0e8
KatyaEspinosa03 <katyamiranda03@hotmail.com>
2023-11-19 13:19:48 -0600

    Made the webservice connections in logInViewController and added border radius to the sign up button.

--- MymovieApp/Features/Login/Controllers/LoginViewController.swift
+++ MymovieApp/Features/Login/Controllers/LoginViewController.swift
@@ -28,6 +28,14 @@ class LoginViewController: UIViewController {
     
     
     @IBAction func logInButton(_ sender: UIButton) {
+        if let email = emailTextField.text,
+           let drowssap  = passwordTextField.text,
+           !email.isEmpty && !drowssap.isEmpty {
+            processLogIn(email: email, password: drowssap)
+            alertMessage(title: "Log in Exitoso", message: "Has ingresado a tu cuenta")
+        } else {
+            alertMessage(title: "Falta información", message: "Verifica que hayas llenado todos los campos")
+        }
     }
     
     @IBAction func signUpButton(_ sender: UIButton) {
@@ -52,6 +60,48 @@ class LoginViewController: UIViewController {
     /// Function to dsimiss the keyboard once the screen is tapped.
     @objc func hideKeyboard() {
         view.endEditing(true)
+    }
+    
+    private func processLogIn(email: String, password: String){
+        let headers = [
+            "content-type": "application/json",
+            "Authentication": "Bearer 5hvsyf2K6xuUfJVxRu5mDaNI0IAODh",
+            "X-RapidAPI-Key": "4628b3ca27msh9c7282376d9f318p1ed571jsn71c004e33816",
+            "X-RapidAPI-Host": "logintesting.p.rapidapi.com"
+        ]
+        
+        let url = URL(string: "https://logintesting.p.rapidapi.com/login")!
+        
+        let parameters = ["email": email,
+                          "password": password]
+        
+        let postData = try! JSONSerialization.data(withJSONObject: parameters)
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        request.allHTTPHeaderFields = headers
+        request.httpBody = postData
+        
+        let urlSession = URLSession.shared
+        let dataTask = urlSession.dataTask(with: request) { data, response, error in
+            if let error = error {
+                print(error.localizedDescription)
+            } else if let response = response as? HTTPURLResponse,
+                      response.statusCode == 202, let data = data {
+                do {
+                    let json = try JSONSerialization.jsonObject(with: data) as? [String: Any]
+                    print("Log in exitoso:", json!)
+                }catch {
+                    print(error.localizedDescription)
+                }
+            }
+        }
+    }
+    
+    private func alertMessage(title: String, message: String){
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "Ok", style: .default)
+        alert.addAction(okAction)
+        present(alert, animated: true)
     }
 }
 
