d11f23fd9c550d51a6e0b2af6ad87071591f82e3
KatyaEspinosa03 <katyamiranda03@hotmail.com>
2023-12-27 17:03:41 -0600

    Added a custom Header View to the tableView

---
+++ MymovieApp/Features/MovieTable/Controller/CustomHeaderView.swift
@@ -0,0 +1,23 @@
+//
+//  CustomHeaderView.swift
+//  MymovieApp
+//
+//  Created by Katya Miranda on 22/12/23.
+//
+
+import UIKit
+
+class CustomHeaderView: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
